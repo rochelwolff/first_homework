5954e7588e72070afc3b08bf0bd11104b8b6d903
rochelwolff <roch238@gmail.com>
2024-07-25 12:50:53 -0400

    first commit of homework

---
+++ functionPrac.js
@@ -0,0 +1,19 @@
+function toCelcius(f){
+
+    return (5/9) * (f-32);
+}
+
+ let f = 77;
+
+console.log (toCelcius(f))
+
+
+function toFarenheit (c){
+   
+    return (9/5) * c +32;
+
+}
+
+let c = 25;
+
+console.log (toFarenheit(c))
